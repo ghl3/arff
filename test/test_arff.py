55dd60d18828f8d5ed539e821c2043a87e79446d
George Lewis <george@lendup.com>
2013-06-05 09:48:44 -0400

    Initial commit of pypi arff package

---
+++ test/test_arff.py
@@ -0,0 +1,106 @@
+'''
+unittests for arff.py
+'''
+
+import unittest
+import os
+
+import arff
+
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# python2/3 compatible unicode
+def u(text):
+    if str == bytes:
+        return text.decode('utf-8')
+    else:
+        # python 3
+        return text
+
+
+class TestArff(unittest.TestCase):
+    def test_read(self):
+        text = u('''@relation diabetics_data
+@attribute hair_color {blonde, black, blue}
+@attribute age real
+@attribute patno integer
+@data
+blonde, 17.2, 1
+blue, 27.2, 2
+blue, 18.2, 3
+''')
+        expected = [
+            ['blonde', 17.2, 1],
+            ['blue', 27.2, 2],
+            ['blue', 18.2, 3],
+            ]
+
+        result = list(arff.loads(text))
+        list_result = [list(row) for row in result]
+        
+        self.assertEqual(list_result, expected)
+        
+        self.assertEqual(result[0].hair_color, 'blonde')
+        self.assertEqual(result[0]['hair_color'], 'blonde')
+        
+    def test_write(self):
+        table = [
+            ['blonde', 17.2, 1],
+            ['blue', 27.2, 2],
+            ['blue', 18.2, 3],
+            ]
+        
+        expected = [
+            '@relation untitled',
+            '@attribute attr0 string',
+            '@attribute attr1 real',
+            '@attribute attr2 integer',
+            '@data',
+            "'blonde',17.2,1",
+            "'blue',27.2,2",
+            "'blue',18.2,3"
+            ]
+        
+        
+        
+        res = arff.dump_lines(table)
+        res = list(res)
+        
+        self.assertEqual(res, expected)
+        
+    def test_files(self):
+        fname = os.path.join(SRC_DIR, 'example.arff')
+        data = [
+            ['blonde', 17.2, 1],
+            ['blue', 27.2, 2],
+            ['blue', 18.2, 3],
+            ]        
+        arff.dump(fname, data, relation='diabetics_data', names=('hair_color', 'age', 'patno'))
+        data = list(arff.load(os.path.join(SRC_DIR, fname)))
+        arff_rows = arff.dumps(data)
+        reparsed_data = list(arff.loads(arff_rows))
+        
+        data = [list(row) for row in data]
+        reparsed_data = [list(row) for row in reparsed_data]
+        
+        self.assertEqual(data, reparsed_data)
+
+    def test_standard_files(self):
+        # this file contains attributes named "class" and comments after
+        # the data
+        fname = os.path.join(SRC_DIR, 'ionosphere.arff')
+        data = list(arff.load(fname))        
+        
+        fname = os.path.join(SRC_DIR, 'sonar.arff')
+        data = list(arff.load(fname))
+
+        # contains nominals with quotes
+        fname = os.path.join(SRC_DIR, 'glass.arff')
+        data = list(arff.load(fname))
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
